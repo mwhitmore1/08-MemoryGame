b4b1a73266256ca84335ded65b437dfb594a8cc6
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-04-29 18:55:49 -0700

    add basic game functionality

---
+++ index.js
@@ -0,0 +1,84 @@
+var app = angular.module('app', ['angular.filter']);
+
+app.controller('appCtrl', function($scope, $filter){
+
+	orderBy = $filter('orderBy');
+
+	var halfCardArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r'];
+	var allCardArray = halfCardArray.concat(halfCardArray);
+
+	$scope.hide = false;
+	
+	var flipped = [] 
+
+	$scope.flip = function(index){
+		var card = $scope.deck[index]
+		// do nothing if the card has already been matched.
+
+		if (card.matched) return;
+		
+		// prevent the same card from being flipped over twice
+		if (flipped.length < 2 && card.hide){
+			
+			// add the card to the list of flipped cards.
+			flipped.push(card)
+			card.hide = !card.hide;
+
+			// if second card flipped, check to see if there is a match and pop an alert 
+			// message if there are no unmatched cards.
+			if (flipped.length == 2){
+				
+				// flip over the cards permanently if they match.
+				if (flipped[0].image == flipped[1].image){
+					for (i in flipped){
+						flipped[i].matched = true;
+					}
+
+					// empty flipped.
+					flipped = [];
+				
+					// check for unmatched cards.
+					for (i in $scope.deck){
+						if (!$scope.deck[i].matched){
+							return;
+						}
+					}
+
+					// if no unmatched cards left alert victory
+					setTimeout(function(){alert("Victory!")}, 1)
+				}	
+			}
+		} else {
+			// hide the two flipped over cards
+			for (i in flipped){
+				flipped[i].hide = true;
+			}
+			// empty the list of flipped cards.
+			flipped = [];
+		}
+	};
+
+	function Card(image){
+		this.image = image;
+		this.hide = true;
+		this.matched = false;
+		this.timeout = 0;
+	}
+
+	$scope.randomOrder = function(a){
+		return -0.5 + Math.random();
+	};
+
+
+	function createDeck(){
+		deck = [];
+		for(i in allCardArray){
+			var newCard = new Card(allCardArray[i]);
+
+			deck[i] = newCard;
+		}
+		return orderBy(deck, $scope.randomOrder, true);
+	}
+
+	$scope.deck = createDeck()
+});
